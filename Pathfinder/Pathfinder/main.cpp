fdddc3028c6228a89a13b42582433a170bbc6b5a
Batuhan Kından <batuhan.kindan@gmail.com>
2020-02-10 13:40:27 +0300

    File Header Comments Added to main.cpp

--- Pathfinder/Pathfinder/main.cpp
+++ Pathfinder/Pathfinder/main.cpp
@@ -1,6 +1,14 @@
+/**
+  ******************************************************************************
+  * @file    main.cpp
+  * @author  Ali Batuhan KINDAN
+  * @date    10.02.2020
+  * @brief   This file contains the test application which demonstrates the AStar
+  *          pathfinding algorithm
+  ******************************************************************************
+  */
+
 #include <iostream>
-
-
 
 int main()
 {
