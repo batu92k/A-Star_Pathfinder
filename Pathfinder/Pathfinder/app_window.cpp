7a0d5a928c00785b200df29cc642bae3ba658368
Batuhan Kından <batuhan.kindan@gmail.com>
2020-02-14 15:31:12 +0300

    Test Drawing Implemented

--- Pathfinder/Pathfinder/app_window.cpp
+++ Pathfinder/Pathfinder/app_window.cpp
@@ -18,6 +18,111 @@
 static void glfw_error_callback(int error, const char* description);
 static void glfw_mouse_btn_callback(GLFWwindow* window, int button, int action, int mods);
 
+float verticolor[] = {
+	 0.5f,  0.5f, 0.0f, 1.0f, 0, 0,
+	 0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0,
+	 -0.5f, -0.5f, 0.0f, 1.0f, 0, 1.0f,
+	 -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f,
+};
+
+unsigned int indices[] = {
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+};
+
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout(location = 0) in vec3 aPos;\n"
+"layout(location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(ourColor, 1.0);\n"
+"}\0";
+
+unsigned int VBO;
+unsigned int vertexShader;
+unsigned int fragmentShader;
+unsigned int shaderProgram;
+unsigned int VAO;
+unsigned int EBO; // element array buffer
+
+char infoLog[512];
+
+void GenerateTestBuffers() 
+{
+	// generate arrays in the beginning
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	glGenVertexArrays(1, &VAO);
+	// 1. bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticolor), verticolor, GL_STATIC_DRAW);
+	// 3. then set our vertex attributes pointers
+	// position has attribute 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color has attribute 1 (has offset of 3 floats from beginning)
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// element buffer
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+}
+
+void GenerateTestShaders() 
+{
+	int  success;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADERS::LINKER_FAILED\n" << infoLog << std::endl;
+	}
+}
+
+void DeleteTestShaders()
+{
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+}
+
 void Start_AppWindow()
 {
 	glfwSetErrorCallback(glfw_error_callback);
@@ -30,7 +135,7 @@ void Start_AppWindow()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "A-Star Pathfinder", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "A-Star Pathfinder", NULL, NULL);
 	if (window == NULL) {
 		return;
 	}
@@ -52,6 +157,9 @@ void Start_AppWindow()
 	std::cout << "GL Version: " << glGetString(GL_VERSION) << std::endl;
 	std::cout << "Shader Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
 
+	GenerateTestBuffers();
+	GenerateTestShaders();
+
 	while (!glfwWindowShouldClose(window))
 	{	
 		int display_w, display_h;
@@ -61,11 +169,16 @@ void Start_AppWindow()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//Draw Here!
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	DeleteTestShaders();
 	glfwDestroyWindow(window);
 	glfwTerminate();
 }
